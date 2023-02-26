75c623d9df250d971382556d482a54a5229b83ef
Erin Beachkofski <erbeach527@gmail.com>
2023-02-25 22:06:44 -0800

    Switched from PostgreSQL to MySQL because my hosting service only has MySQL.

--- server/plant_model.js
+++ server/plant_model.js
@@ -1,23 +1,24 @@
 const config = require('../config.json');
 
-const Pool = require('pg').Pool
-const pool = new Pool({
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  connectionLimit: 10,
   user: config.user,
   host: config.host,
   database: config.database,
   password: config.password,
-  port: config.port,
 });
 
 // Postgres connection 
 const getPlants = () => {
     return new Promise(function(resolve, reject) {
-      pool.query('SELECT * FROM plant', (error, results) => {
+      connection.query('SELECT * FROM plant', (error, results, fields) => {
         if (error) {
           reject(error)
         }
         
-        resolve(results.rows);
+        resolve(results);
       })
     });
   }
